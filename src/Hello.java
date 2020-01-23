ac907806547f08181a362f00a67c77a36f5e2d9a
m-tartag <mattbrs51@gmail.com>
2020-01-23 12:21:10 -0500

    update

--- src/Hello.java
+++ src/Hello.java
@@ -4,9 +4,15 @@ public class Hello {
 
         for (int i = 0; i < 10; i++ ) {
             System.out.println("Hello World");
-
         }
 
+        int NUMBER = 100;
+
+        int MAXX = Integer.MAX_VALUE;
+        byte b_MAXX = Byte.MAX_VALUE;
+
+
+        System.out.println(b_MAXX);
 
     }
 
