e4abefc24d27f5108fd79690ea7d0f9c514b34c5
m-tartag <mattbrs51@gmail.com>
2020-01-28 12:30:04 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,57 @@
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 9900, 13, 100);
-        displayHighScorePosition("Matthew", calculateHighScorePosition(1500));
-        displayHighScorePosition("Richard", calculateHighScorePosition(900));
-        displayHighScorePosition("Nicholas", calculateHighScorePosition(400));
-        displayHighScorePosition("Luigi", calculateHighScorePosition(50));
+        Car porsche = new Car();
+        porsche.setModel("Ayy");
 
-        ArrayList<String> al=new ArrayList<String>();
-        al.add("John");
-        al.add("George");
-        al.add("Ringo");
-        al.add("Paul");
-        al.add("Pete");
-        al.add("Michael");
-        for (String names : al) {
-            System.out.println(names);
-        }
-    }
+        System.out.println("Model: " + porsche.getModel());
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 2000;
-            System.out.println("Your Final Score Was: " + finalScore);
-        }
-
-    }
-
-    public static int calculateHighScorePosition(int score) {
-        if (score > 1000) {
-            return 1;
-        }
-        else if (score > 500 && score < 1000 ) {
-            return 2;
-        }
-        else if (score > 100 && score < 500 ) {
-            return 3;
-        }
-        return 4;
-    }
-
-    public static int displayHighScorePosition(String name, int position) {
-        System.out.println(name + " managed to secure rank: " + position);
-        return 0;
-    }
-//    public static int sortHighScores(int[] ARRAY) {
+//        calculateScore(true, 9900, 13, 100);
+//        displayHighScorePosition("Matthew", calculateHighScorePosition(1500));
+//        displayHighScorePosition("Richard", calculateHighScorePosition(900));
+//        displayHighScorePosition("Nicholas", calculateHighScorePosition(400));
+//        displayHighScorePosition("Luigi", calculateHighScorePosition(50));
+//
+//        ArrayList<String> al=new ArrayList<String>();
+//        al.add("John");
+//        al.add("George");
+//        al.add("Ringo");
+//        al.add("Paul");
+//        al.add("Pete");
+//        al.add("Michael");
+//        for (String names : al) {
+//            System.out.println(names);
+//        }
+//    }
+//
+//    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+//        if (gameOver) {
+//            int finalScore = score + (levelCompleted * bonus);
+//            finalScore += 2000;
+//            System.out.println("Your Final Score Was: " + finalScore);
+//        }
 //
 //    }
+//
+//    public static int calculateHighScorePosition(int score) {
+//        if (score > 1000) {
+//            return 1;
+//        }
+//        else if (score > 500 && score < 1000 ) {
+//            return 2;
+//        }
+//        else if (score > 100 && score < 500 ) {
+//            return 3;
+//        }
+//        return 4;
+//    }
+//
+//    public static int displayHighScorePosition(String name, int position) {
+//        System.out.println(name + " managed to secure rank: " + position);
+//        return 0;
+//    }
+////    public static int sortHighScores(int[] ARRAY) {
+////
+    }
 }
--- src/Math.java
+++ src/Math.java
@@ -5,6 +5,7 @@ public class Math {
         public static void main(String[] args) {
             Math myObj = new Math();
             Math myObj2 = new Math();
+            myObj.x = 42;
             System.out.println(myObj.x);
             System.out.println(myObj.x);
 
