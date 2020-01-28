02d968566d38f0c2079275f0c07f6262c0246e21
m-tartag <mattbrs51@gmail.com>
2020-01-28 11:29:01 -0500

    update

---
+++ src/Math.java
@@ -0,0 +1,14 @@
+public class Math {
+
+        int x = 5;
+
+        public static void main(String[] args) {
+            Math myObj = new Math();
+            Math myObj2 = new Math();
+            System.out.println(myObj.x);
+            System.out.println(myObj.x);
+
+        }
+
+
+}
